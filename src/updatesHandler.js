30b46efb32e6ff25ae6b30f2f6972e88d84416d9
andrii.smahin <andrii.smahin@masterofcode.com>
2022-03-04 17:52:28 +0200

    fixed

--- src/updatesHandler.js
+++ src/updatesHandler.js
@@ -6,11 +6,7 @@ const logger = require('./logger');
 
 module.exports = (api, updateInfo) => {
   if (updateInfo.chats && updateInfo.chats.length === 0) return;
-  logger.info(
-    `Receive new updates from ${
-      updateInfo?.chats[0]?.title || 'untitled update'
-    }`,
-  );
+  logger.info(`Receive new updates from ${updateInfo.chats[0].title}`);
   const redirectThisUpdates = isRedirectThisUpdates(updateInfo.updates);
   if (redirectThisUpdates.length && isWorkingTime()) {
     logger.info('Found new messages for forwarding');
