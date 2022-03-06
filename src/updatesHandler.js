5777b2fc0ebf179cd53b0fd437ddde294d644904
andrii.smahin <andrii.smahin@masterofcode.com>
2022-03-06 20:54:53 +0200

    added new messages filter

--- src/updatesHandler.js
+++ src/updatesHandler.js
@@ -3,7 +3,10 @@ const isRedirectThisUpdates = require('./isRedirectThisUpdates');
 const logger = require('./logger');
 
 module.exports = (api, updateInfo) => {
-  if (updateInfo.chats && updateInfo.chats.length === 0) return;
+  const newMessageUpdates = updateInfo.updates.filter(
+    (anUpdate) => anUpdate._ === 'updateNewChannelMessage',
+  );
+  if (newMessageUpdates && newMessageUpdates.length === 0) return;
   const chatTitles = updateInfo.chats.map((aChat) => aChat.title).join(' - ');
   logger.info(`Receive new updates from "${chatTitles}"`);
   const redirectThisUpdates = isRedirectThisUpdates(updateInfo.updates);
