8f5bbf16b7253b28c766c7a2bd0062d8db5c091f
andrii.smahin <andrii.smahin@masterofcode.com>
2022-03-07 13:26:36 +0200

    fix port

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const requestHandler = require('./requestHandler');
 
 module.exports = () => {
-  http.createServer(requestHandler).listen(3000, () => {
-    console.log(`Server successfully started on port ${3000}`);
+  http.createServer(requestHandler).listen(3001, () => {
+    console.log(`Server successfully started on port ${3001}`);
   });
 };
