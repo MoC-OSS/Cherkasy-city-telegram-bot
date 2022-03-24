15661b136ef74791fded8678ac35841cea162459
andrii.smahin <andrii.smahin@masterofcode.com>
2022-03-24 11:20:39 +0200

    fixed db model

--- src/db/migrations/20220319082839_create_jobs_table.js
+++ src/db/migrations/20220319082839_create_jobs_table.js
@@ -6,7 +6,7 @@ exports.up = async (knex) => {
   await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
   return knex.schema.createTable('jobs', (table) => {
     table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
-    table.integer('creator_id').notNullable();
+    table.string('creator_id', 12).notNullable();
     table.increments('count_id');
     table.string('compony_name', 1023);
     table.string('settlement', 1023);
