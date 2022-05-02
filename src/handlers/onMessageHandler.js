e644f502e699cf75d648e3628363664129cce813
mykolasarancha <mykola.sarancha@masterofcode.com>
2022-05-02 09:51:50 +0300

    hotfix2

--- src/handlers/onMessageHandler.js
+++ src/handlers/onMessageHandler.js
@@ -10,12 +10,19 @@ const messages = require('../messages');
  * @param {GrammyContext} ctx
  * */
 module.exports = async (ctx) => {
-  try {
-    const keyboard = new Keyboard().text(messages.buttons.shareJob);
-    ctx.reply(messages.default, {
+  const keyboard = new Keyboard().text(messages.buttons.shareJob);
+  ctx
+    .reply(messages.default, {
       one_time_keyboard: true,
       resize_keyboard: true,
       keyboard: keyboard.build(),
+    })
+    .then(function (resp) {
+      // ...snip...
+    })
+    .catch(function (error) {
+      if (error.response && error.response.statusCode === 403) {
+        // ...snip...
+      }
     });
-  } catch {}
 };
